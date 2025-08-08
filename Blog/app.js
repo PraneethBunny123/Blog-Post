d8851feb7959ba7e7c9c3d8d6dd2a995f1720630
praneeth <gp.gunturu@gmail.com>
2025-08-07 22:59:58 -0500

    get, redirect, 404s

--- Blog/app.js
+++ Blog/app.js
@@ -2,9 +2,25 @@ const express = require('express')
 
 const app = express()
 
+// listen
 app.listen(3000)
 
+
+// get req
 app.get('/', (req, res) => {
-    console.log(req.url, req.method)
-    res.send('<p>Home express</p>')
+    res.sendFile('./pages/index.html', {root: __dirname})
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('./pages/about.html', {root: __dirname})
+})
+
+// redirects
+app.get('/about-me', (req, res) => {
+    res.redirect('/about')
+})
+
+// 404
+app.use((req, res) => {
+    res.status(404).sendFile('./pages/404.html', {root: __dirname})
 })
