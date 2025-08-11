9cbb5941aadba75f5b6d1878bc8451eeaab6035a
praneeth <gp.gunturu@gmail.com>
2025-08-11 17:09:51 -0500

    using ejs files instead of static html. setup the app and created the ejs files and gave basic styles

--- Blog/app.js
+++ Blog/app.js
@@ -2,25 +2,40 @@ const express = require('express')
 
 const app = express()
 
+// register view engine
+app.set('view engine', 'ejs')
+
+// change default views folder to pages
+app.set('views', './pages')
+
 // listen
 app.listen(3000)
 
 
 // get req
 app.get('/', (req, res) => {
-    res.sendFile('./pages/index.html', {root: __dirname})
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs})
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./pages/about.html', {root: __dirname})
+    res.render('about', {title: 'About'})
 })
 
 // redirects
-app.get('/about-me', (req, res) => {
-    res.redirect('/about')
+// app.get('/about-me', (req, res) => {
+//     res.redirect('/about')
+// })
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'})
 })
 
 // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./pages/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 })
